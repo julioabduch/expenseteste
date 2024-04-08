100b7aa5c31a55539e8c29234143184f3204cd8c
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-04-08 01:26:23 +0000

    Updating to latest FlutterFlow output.

--- lib/components/addbudget_model.dart
+++ lib/components/addbudget_model.dart
@@ -1,9 +1,7 @@
 import '/backend/sqlite/sqlite_manager.dart';
-import '/flutter_flow/flutter_flow_drop_down.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import '/flutter_flow/form_field_controller.dart';
 import 'addbudget_widget.dart' show AddbudgetWidget;
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -12,9 +10,6 @@ import 'package:provider/provider.dart';
 class AddbudgetModel extends FlutterFlowModel<AddbudgetWidget> {
   ///  State fields for stateful widgets in this component.
 
-  // State field(s) for DropDown widget.
-  String? dropDownValue;
-  FormFieldController<String>? dropDownValueController;
   // State field(s) for name widget.
   FocusNode? nameFocusNode;
   TextEditingController? nameController;
--- lib/components/addbudget_widget.dart
+++ lib/components/addbudget_widget.dart
@@ -1,9 +1,7 @@
 import '/backend/sqlite/sqlite_manager.dart';
-import '/flutter_flow/flutter_flow_drop_down.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import '/flutter_flow/form_field_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -52,59 +50,6 @@ class _AddbudgetWidgetState extends State<AddbudgetWidget> {
       child: Column(
         mainAxisSize: MainAxisSize.max,
         children: [
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: FutureBuilder<List<GetCategoriaRow>>(
-              future: SQLiteManager.instance.getCategoria(),
-              builder: (context, snapshot) {
-                // Customize what your widget looks like when it's loading.
-                if (!snapshot.hasData) {
-                  return Center(
-                    child: SizedBox(
-                      width: 50.0,
-                      height: 50.0,
-                      child: CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation<Color>(
-                          FlutterFlowTheme.of(context).primary,
-                        ),
-                      ),
-                    ),
-                  );
-                }
-                final dropDownGetCategoriaRowList = snapshot.data!;
-                return FlutterFlowDropDown<String>(
-                  controller: _model.dropDownValueController ??=
-                      FormFieldController<String>(null),
-                  options:
-                      dropDownGetCategoriaRowList.map((e) => e.name).toList(),
-                  onChanged: (val) =>
-                      setState(() => _model.dropDownValue = val),
-                  width: 300.0,
-                  height: 56.0,
-                  textStyle: FlutterFlowTheme.of(context).bodyMedium.override(
-                        fontFamily: 'Readex Pro',
-                        letterSpacing: 0.0,
-                      ),
-                  hintText: 'Please select...',
-                  icon: Icon(
-                    Icons.keyboard_arrow_down_rounded,
-                    color: FlutterFlowTheme.of(context).secondaryText,
-                    size: 24.0,
-                  ),
-                  fillColor: FlutterFlowTheme.of(context).secondaryBackground,
-                  elevation: 2.0,
-                  borderColor: FlutterFlowTheme.of(context).alternate,
-                  borderWidth: 2.0,
-                  borderRadius: 8.0,
-                  margin: EdgeInsetsDirectional.fromSTEB(16.0, 4.0, 16.0, 4.0),
-                  hidesUnderline: true,
-                  isOverButton: true,
-                  isSearchable: false,
-                  isMultiSelect: false,
-                );
-              },
-            ),
-          ),
           Padding(
             padding: EdgeInsets.all(10.0),
             child: TextFormField(
